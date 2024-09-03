3414619fa0c45bc3d06bb9fd50934db97013e51d
2ombe <jtuombe@gmail.com>
2024-09-03 13:43:27 +0200

    save

--- personal/src/components/Projects.js
+++ personal/src/components/Projects.js
@@ -1,8 +1,13 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/vegetables.jpg";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+import projImg2 from "../assets/img/training.jpg";
+import projImg3 from "../assets/img/extent.jpeg";
+import projImg4 from "../assets/img/driper.PNG";
+import projImg5 from "../assets/img/agribus.PNG";
+import projImg6 from "../assets/img/agrifi.PNG";
+import projImg7 from "../assets/img/cropman.PNG";
+import projImg8 from "../assets/img/conc.PNG";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
@@ -31,31 +36,31 @@ export const Projects = () => {
       title: "Infrastructure Development",
       description:
         "Building and maintaining critical agricultural infrastructure such as irrigation systems, roads, storage facilities, and processing plants.",
-      imgUrl: projImg1,
+      imgUrl: projImg4,
     },
     {
       title: "Market Access",
       description:
         "Facilitating access to markets for farmers, including the development of transportation networks and market information systems.",
-      imgUrl: projImg2,
+      imgUrl: projImg5,
     },
     {
       title: "Credit and Finance",
       description:
         "Providing farmers with access to affordable credit and financial services to invest in their farms and purchase necessary inputs.",
-      imgUrl: projImg2,
+      imgUrl: projImg6,
     },
     {
       title: "Crop and Livestock Management",
       description:
         "Promoting sustainable farming practices, including crop rotation, intercropping, integrated pest management, and livestock management.",
-      imgUrl: projImg3,
+      imgUrl: projImg7,
     },
     {
       title: "Soil and Water Conservation",
       description:
         "Implementing soil conservation techniques, watershed management, and sustainable water use practices to protect natural resources.",
-      imgUrl: projImg3,
+      imgUrl: projImg8,
     },
   ];
 
